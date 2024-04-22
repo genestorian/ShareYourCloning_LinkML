3b0ddbb1038c7c9bb3f6d2b0b1310e11ffb261ad
Manuel Lera Ramirez <manulera14@gmail.com>
2024-04-22 21:34:35 +0200

    small changes

--- pydantic_model.py
+++ pydantic_model.py
@@ -1,86 +1,104 @@
-from __future__ import annotations
-from datetime import datetime, date
-from enum import Enum
-
-from decimal import Decimal
-from typing import List, Dict, Optional, Any, Union
-from pydantic import BaseModel as BaseModel, Field, validator
+from __future__ import annotations 
+from datetime import (
+    datetime,
+    date
+)
+from decimal import Decimal 
+from enum import Enum 
 import re
 import sys
-if sys.version_info >= (3, 8):
-    from typing import Literal
+from typing import (
+    Any,
+    List,
+    Literal,
+    Dict,
+    Optional,
+    Union
+)
+from pydantic.version import VERSION  as PYDANTIC_VERSION 
+if int(PYDANTIC_VERSION[0])>=2:
+    from pydantic import (
+        BaseModel,
+        ConfigDict,
+        Field,
+        field_validator,
+        validator
+    )
 else:
-    from typing_extensions import Literal
-
+    from pydantic import (
+        BaseModel,
+        Field,
+        validator
+    )
 
 metamodel_version = "None"
 version = "None"
 
+
 class WeakRefShimBaseModel(BaseModel):
-   __slots__ = '__weakref__'
+    __slots__ = '__weakref__'
 
-class ConfiguredBaseModel(WeakRefShimBaseModel,
-                validate_assignment = True,
-                validate_all = True,
-                underscore_attrs_are_private = True,
-                extra = 'forbid',
-                arbitrary_types_allowed = True,
-                use_enum_values = True):
-
+class ConfiguredBaseModel(WeakRefShimBaseModel):
     pass
 
-        
 
 class RepositoryName(str, Enum):
-    
     # Addgene
     addgene = "addgene"
     # GenBank
     genbank = "genbank"
-    
-    
 
-class SourceType(str, Enum):
-    
-    
-    repository_id = "repository_id"
-    
-    file = "file"
-    
-    restriction = "restriction"
-    
-    ligation = "ligation"
-    
-    PCR = "PCR"
-    
-    homologous_recombination = "homologous_recombination"
-    
-    gibson_assembly = "gibson_assembly"
-    
-    restriction_and_ligation = "restriction_and_ligation"
-    
-    genome_coordinates = "genome_coordinates"
-    
-    manually_typed = "manually_typed"
-    
-    
 
 class SequenceFileFormat(str, Enum):
-    
-    
     fasta = "fasta"
-    
     genbank = "genbank"
-    
     snapgene = "snapgene"
-    
-    
+
 
 class NamedThing(ConfiguredBaseModel):
-    
     id: int = Field(..., description="""A unique identifier for a thing""")
-    
-    
+
+
+class Sequence(NamedThing):
+    """
+    Represents a sequence
+    """
+    id: int = Field(..., description="""A unique identifier for a thing""")
+    type: Literal["Sequence"] = Field("Sequence", description="""The type of the source""")
+
+
+class TextFileSequence(Sequence):
+    """
+    A sequence (may have features) defined by the content of a text file
+    """
+    sequence_file_format: SequenceFileFormat = Field(..., description="""The format of a sequence file""")
+    file_content: Optional[str] = Field(None)
+    overhang_crick_3prime: Optional[int] = Field(None, description="""Taken from pydna's `dseq::ovhg`An integer describing the length of the crick strand overhang in the 5' of the molecule, or 3' of the crick strand""")
+    overhang_watson_3prime: Optional[int] = Field(None, description="""The equivalent of `overhang_crick_3prime` but for the watson strand""")
+    id: int = Field(..., description="""A unique identifier for a thing""")
+    type: Literal["TextFileSequence"] = Field("TextFileSequence", description="""The type of the source""")
+
+
+class Primer(NamedThing):
+    """
+    An oligonucleotide or primer
+    """
+    id: int = Field(..., description="""A unique identifier for a thing""")
+    name: Optional[str] = Field(None, description="""A human-readable name for a thing""")
+    sequence: Optional[str] = Field(None)
+
+    @validator('sequence', allow_reuse=True)
+    def pattern_sequence(cls, v):
+        pattern=re.compile(r"^[acgtACGT]+$")
+        if isinstance(v,list):
+            for element in v:
+                if not pattern.match(element):
+                    raise ValueError(f"Invalid sequence format: {element}")
+        elif isinstance(v,str):
+            if not pattern.match(v):
+                raise ValueError(f"Invalid sequence format: {v}")
+        return v
+
 
 class Source(NamedThing):
     """
@@ -88,24 +106,20 @@ class Source(NamedThing):
     """
     input: Optional[List[int]] = Field(default_factory=list, description="""Identifiers of the sequences that are an input to this source. If the source represents external import of a sequence, it's empty.""")
     output: Optional[int] = Field(None, description="""Identifier of the sequence that is the output of this source.""")
-    type: SourceType = Field(..., description="""The type of the source""")
-    kind: str = Field(..., description="""The kind entity (always equal to \"source\"). Should probably be removed.""")
+    type: Literal["Source"] = Field("Source", description="""The type of the source""")
     id: int = Field(..., description="""A unique identifier for a thing""")
-    
-    
+
 
 class ManuallyTypedSource(Source):
     """
     Represents the source of a sequence that is manually typed by the user
     """
-    user_input: Optional[str] = Field(None)
+    user_input: str = Field(...)
     input: Optional[List[int]] = Field(default_factory=list, description="""Identifiers of the sequences that are an input to this source. If the source represents external import of a sequence, it's empty.""")
     output: Optional[int] = Field(None, description="""Identifier of the sequence that is the output of this source.""")
-    type: SourceType = Field(..., description="""The type of the source""")
-    kind: str = Field(..., description="""The kind entity (always equal to \"source\"). Should probably be removed.""")
+    type: Literal["ManuallyTypedSource"] = Field("ManuallyTypedSource", description="""The type of the source""")
     id: int = Field(..., description="""A unique identifier for a thing""")
-    
-    
+
     @validator('user_input', allow_reuse=True)
     def pattern_user_input(cls, v):
         pattern=re.compile(r"^[acgtACGT]+$")
@@ -117,22 +131,20 @@ class ManuallyTypedSource(Source):
             if not pattern.match(v):
                 raise ValueError(f"Invalid user_input format: {v}")
         return v
-    
+
 
 class UploadedFileSource(Source):
     """
     Represents the source of a sequence that is uploaded as a file
     """
+    sequence_file_format: SequenceFileFormat = Field(..., description="""The format of a sequence file""")
     file_name: Optional[str] = Field(None, description="""The name of the file""")
     index_in_file: Optional[int] = Field(None, description="""The index of the sequence in the file""")
-    file_format: Optional[SequenceFileFormat] = Field(None, description="""The format of the file""")
     input: Optional[List[int]] = Field(default_factory=list, description="""Identifiers of the sequences that are an input to this source. If the source represents external import of a sequence, it's empty.""")
     output: Optional[int] = Field(None, description="""Identifier of the sequence that is the output of this source.""")
-    type: SourceType = Field(..., description="""The type of the source""")
-    kind: str = Field(..., description="""The kind entity (always equal to \"source\"). Should probably be removed.""")
+    type: Literal["UploadedFileSource"] = Field("UploadedFileSource", description="""The type of the source""")
     id: int = Field(..., description="""A unique identifier for a thing""")
-    
-    
+
 
 class RepositoryIdSource(Source):
     """
@@ -142,11 +154,9 @@ class RepositoryIdSource(Source):
     repository_id: str = Field(..., description="""The id of the sequence in the repository""")
     input: Optional[List[int]] = Field(default_factory=list, description="""Identifiers of the sequences that are an input to this source. If the source represents external import of a sequence, it's empty.""")
     output: Optional[int] = Field(None, description="""Identifier of the sequence that is the output of this source.""")
-    type: SourceType = Field(..., description="""The type of the source""")
-    kind: str = Field(..., description="""The kind entity (always equal to \"source\"). Should probably be removed.""")
+    type: Literal["RepositoryIdSource"] = Field("RepositoryIdSource", description="""The type of the source""")
     id: int = Field(..., description="""A unique identifier for a thing""")
-    
-    
+
 
 class GenomeCoordinatesSource(Source):
     """
@@ -161,16 +171,16 @@ class GenomeCoordinatesSource(Source):
     strand: int = Field(..., description="""The strand of the sequence in the sequence accession, should be 1 or -1""")
     input: Optional[List[int]] = Field(default_factory=list, description="""Identifiers of the sequences that are an input to this source. If the source represents external import of a sequence, it's empty.""")
     output: Optional[int] = Field(None, description="""Identifier of the sequence that is the output of this source.""")
-    type: SourceType = Field(..., description="""The type of the source""")
-    kind: str = Field(..., description="""The kind entity (always equal to \"source\"). Should probably be removed.""")
+    type: Literal["GenomeCoordinatesSource"] = Field("GenomeCoordinatesSource", description="""The type of the source""")
     id: int = Field(..., description="""A unique identifier for a thing""")
-    
-    
 
 
 # Update forward refs
 # see https://pydantic-docs.helpmanual.io/usage/postponed_annotations/
 NamedThing.update_forward_refs()
+Sequence.update_forward_refs()
+TextFileSequence.update_forward_refs()
+Primer.update_forward_refs()
 Source.update_forward_refs()
 ManuallyTypedSource.update_forward_refs()
 UploadedFileSource.update_forward_refs()
