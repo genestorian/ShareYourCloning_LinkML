89f0e5dc925d68e1497fe6264e39d15d36fcf0c3
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-05-08 17:39:06 +0100

    fix cloning strategy

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,13 +4,13 @@ import os
 import glob
 import unittest
 
-from linkml_runtime.loaders import yaml_loader
+from linkml_runtime.loaders import json_loader
 from shareyourcloning_linkml.datamodel import CloningStrategy
 
 ROOT = os.path.join(os.path.dirname(__file__), "..")
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
 
-EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR, "*.yaml"))
+EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR, "*.json"))
 
 
 class TestData(unittest.TestCase):
@@ -19,5 +19,5 @@ class TestData(unittest.TestCase):
     def test_data(self):
         """Date test."""
         for path in EXAMPLE_FILES:
-            obj = yaml_loader.load(path, target_class=CloningStrategy)
+            obj = json_loader.load(path, target_class=CloningStrategy)
             assert obj
